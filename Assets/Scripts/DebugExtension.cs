1287a438cac277cc81fe4d15d683581f742ed910
rhedgeco <rhedgeco@gmail.com>
2020-11-23 10:49:37 -0800

    Added mesh drawer and updated snapshot

--- Assets/Scripts/DebugExtension.cs
+++ Assets/Scripts/DebugExtension.cs
@@ -65,5 +65,30 @@ namespace DebugExtensions
             DrawCircle(center, Vector3.right, radius, color, duration, depthTest, resolution);
             DrawCircle(center, Vector3.up, radius, color, duration, depthTest, resolution);
         }
+
+        public static void DrawMesh(Mesh mesh, Vector3 position, Color? color = null,
+            Vector3? scale = null, Quaternion? rotation = null, float duration = 0.0f, bool depthTest = true)
+        {
+            Vector3 p1, p2, p3;
+            for (int i = 0; i < mesh.triangles.Length; i += 3)
+            {
+                p1 = mesh.vertices[mesh.triangles[i + 0]];
+                p2 = mesh.vertices[mesh.triangles[i + 1]];
+                p3 = mesh.vertices[mesh.triangles[i + 2]];
+                p1 = (rotation ?? Quaternion.identity) * p1;
+                p2 = (rotation ?? Quaternion.identity) * p2;
+                p3 = (rotation ?? Quaternion.identity) * p3;
+                p1.Scale(scale ?? Vector3.one);
+                p2.Scale(scale ?? Vector3.one);
+                p3.Scale(scale ?? Vector3.one);
+                p1 += position;
+                p2 += position;
+                p3 += position;
+                
+                Debug.DrawLine(p1, p2, color ?? Color.white, duration, depthTest);
+                Debug.DrawLine(p2, p3, color ?? Color.white, duration, depthTest);
+                Debug.DrawLine(p3, p1, color ?? Color.white, duration, depthTest);
+            }
+        }
     }
 }
--- Assets/Scripts/ExampleDebugDraw.cs
+++ Assets/Scripts/ExampleDebugDraw.cs
@@ -4,10 +4,13 @@ using DebugExtensions;
 [ExecuteInEditMode]
 public class ExampleDebugDraw : MonoBehaviour
 {
+    [SerializeField] private Mesh _mesh;
+    
     void Update()
     {
+        DebugExtension.DrawMesh(_mesh, new Vector3(0, 2, 0), Color.cyan, Vector3.one * 0.75f);
         DebugExtension.DrawWireSphere(new Vector3(1, -1f, 0), 0.5f, Color.green);
-        DebugExtension.DrawCircle(new Vector3(-0.75f, -1.25f, 0), new Vector3(1, 1, 1), 1, Color.magenta);
+        DebugExtension.DrawCircle(new Vector3(-0.75f, -1.25f, 0), new Vector3(1, 1, 1), 0.75f, Color.magenta);
         DebugExtension.DrawRectangularPrism(new Vector3(0, 0f, 0.5f), new Vector3(2f, 1, -0.5f),
             Color.blue);
         DebugExtension.DrawCube(new Vector3(-1.5f, 0.5f, 0), 1, Color.red);
